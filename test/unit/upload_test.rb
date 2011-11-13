d115069a1eb6322a3351b9b98b20af500fbe4b51
Nicholas Canzoneri <ac3522@gmail.com>
2011-11-13 16:00:12 -0500

    Adding tests for upload model

--- test/unit/upload_test.rb
+++ test/unit/upload_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UploadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "upload time is required" do
+    upload = Upload.new
+    upload.contents = "the contents"
+    assert !upload.save
+  end
+
+  test "contents is required" do
+    upload = Upload.new
+    upload.upload_time = Time.now
+    assert !upload.save
+  end
+
+  test "having required fields marks as valid" do
+    upload = Upload.new
+    upload.upload_time = Time.now
+    upload.contents = "the contents"
+    assert upload.valid?
+  end
 end
