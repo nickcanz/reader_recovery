87c9d4b1f2596ed2ddaccc85e6cab8bd26d3e4f4
Nicholas Canzoneri <ac3522@gmail.com>
2011-11-13 01:45:41 -0500

    Creating upload records in the database

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,12 @@
 class UploadsController < ApplicationController
   def create
     f = params[:upload][:file]
+    upload_record = Upload.create(:upload_time => Time.now.utc, :contents => f.read.to_s)
     redirect_to uploads_path
   end
 
   def index
+    @uploads = Upload.all
     render :index
   end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,4 @@
 class Upload < ActiveRecord::Base
+  validates :upload_time, :presence => true
+  validates :contents, :presence => true
 end
