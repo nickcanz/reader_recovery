59961bfa83f145ef1adc7009ca041c651ce222cb
Nicholas Canzoneri <ac3522@gmail.com>
2011-11-14 00:38:27 -0500

    Adding more upload controller tests

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,9 +2,7 @@ class UploadsController < ApplicationController
   def create
     f = params[:upload][:file]
     upload_record = Upload.create(:upload_time => Time.now.utc, :contents => f.read.to_s)
-
     notes = JSON.parse(upload_record.contents)
-
     redirect_to uploads_path
   end
 
--- test/functional/uploads_controller_test.rb
+++ test/functional/uploads_controller_test.rb
@@ -11,4 +11,13 @@ class UploadsControllerTest < ActionController::TestCase
 
     assert_equal Upload.all.length, actual_uploads.length
   end
+
+  test "creates upload from file" do
+    sample_file = File.join(File.expand_path(File.dirname(__FILE__)), 'sample_upload.json')
+    post :create, { :upload => { :file => Rack::Test::UploadedFile.new(sample_file, 'application/json') } }
+    assert_response :redirect
+    assert_redirected_to(:controller => 'uploads', :action => 'index')
+    upload = Upload.find(:all, :order => 'created_at desc', :limit => 1).first
+    assert_equal "{ \"is_sample\": true }\n", upload.contents
+  end
 end
