1987d554cb14eddb853e14c1409465f7fdbabccf
Nicholas Canzoneri <ac3522@gmail.com>
2011-12-01 22:23:09 -0500

    Limiting notes to five

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,6 @@
 class NotesController < ApplicationController
   def index
-
-    @notes = params[:upload_id].nil?  ? Note.all : Note.where(:upload_id => params[:upload_id])
+    @notes = params[:upload_id].nil?  ? Note.all(:limit => 5) : Note.where(:upload_id => params[:upload_id]).limit(5)
     render :index
   end
 end
