dfe857190b5f82121cf35d8fb58a088a5a97db3f
Nicholas Canzoneri <ac3522@gmail.com>
2011-12-12 22:05:27 -0500

    Adding links on main tags page. Creating show method for tags to show all notes on a tag.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,9 @@ class TagsController < ApplicationController
     @tags = Tag.all
     render :index
   end
+
+  def show
+    @tag = Tag.find(params[:id])
+    render :show
+  end
 end
--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
-  test "index page displays all tags" do
+  test "Tags#index page displays all tags" do
     get :index
 
     assert_response :success
@@ -10,5 +10,18 @@ class TagsControllerTest < ActionController::TestCase
     assert_not_nil actual_tags
 
     assert_equal actual_tags.count, Tag.all.count
+  end
+
+  test "Tags#show assigns tag var" do
+    t = Tag.create(:name => 'The tag')
+
+    get :show, { :id => t.id }
+
+    assert_response :success
+    actual_tag = assigns(:tag)
+
+    assert_not_nil actual_tag
+
+    assert_equal "The tag", actual_tag.name
   end
 end
